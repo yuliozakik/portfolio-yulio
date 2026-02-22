dd77123af7b82f42d1ac5330b3cd654801b17411
yuliozakik <yuliozaki16@gmail.com>
2026-02-22 21:26:19 +0700

    New landing page porto

--- src/app/app.ts
+++ src/app/app.ts
@@ -3,7 +3,7 @@ import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [],
   templateUrl: './app.html',
   styleUrl: './app.css'
 })
